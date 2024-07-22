3a875df8d383c34d676548d0a24d5071eb7e8289
moshe bengi <moshbengi@gmail.com>
2024-07-22 23:29:21 +0300

    can add betting interactive or use csv as before

--- main.py
+++ main.py
@@ -28,16 +28,35 @@ class Bet:  # class for the bet of every person
 
 
 if __name__ == '__main__':
+    data_type = input("1 to enter betting to csv or 2 to write here\n")
     betting = []
-    df = pd.read_csv(FILE_NAME)
-    first_team_name = df.columns[1]
-    second_team_name = df.columns[2]
-    for index, row in df.iterrows():
-        if row[first_team_name] != row[second_team_name]:
-            winner = first_team_name if row[first_team_name] > row[second_team_name] else second_team_name
-        else:
-            winner = row['winner']
-        betting.append(Bet(row[first_team_name], row[second_team_name], winner, row['name']))
+    if data_type == "1":
+        df = pd.read_csv(FILE_NAME)
+        first_team_name = df.columns[1]
+        second_team_name = df.columns[2]
+        for index, row in df.iterrows():
+            if row[first_team_name] != row[second_team_name]:
+                winner = first_team_name if row[first_team_name] > row[second_team_name] else second_team_name
+            else:
+                winner = row['winner']
+            betting.append(Bet(row[first_team_name], row[second_team_name], winner, row['name']))
+    else:
+        first_team_name = input("first team name: \n")
+        second_team_name = input("second team name: \n")
+        user = 1
+        while True:
+            user = input("next user name: (0 if no more users)\n")
+            if user == "0":
+                break
+            first_team_score = input("how many " + first_team_name + " will score?\n")
+            second_team_score = input("how many " + second_team_name + " will score?\n")
+            if first_team_score != second_team_score:
+                winner = first_team_name if first_team_name > second_team_name else second_team_name
+            else:
+                winner = input(("who will win? 1 is " + first_team_name + " 2 is " + second_team_name + "\n"))
+                winner = first_team_name if winner == '1' else second_team_name
+            betting.append(Bet(int(first_team_score), int(second_team_score), winner, user))
+    input("press enter when game is over\n")
     print("GAME IS OVER!")
     time.sleep(2)
     first_score = int(input("how many " + first_team_name + " scored?\n"))
@@ -51,7 +70,7 @@ if __name__ == '__main__':
 
     print("the grades are:")
     time.sleep(2)
-    max_points = -np.Inf
+    max_points = -np.inf
     for b in betting:
         b.calculate_grade(actual)
         if b.points > max_points:
